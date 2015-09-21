e76b4ab60fc86c13abfe1c88a4ad38f4c56cf8d9
Andrew Shaffer <darawk@gmail.com>
2015-09-20 18:20:32 -0700

    some updates

--- src/index.js
+++ src/index.js
@@ -22,83 +22,111 @@ const DESTROY = 'DESTROY_EPHEMERAL'
 const localReducers = {}
 
 /**
- * Ephemeral state management
+ * Ephemeral state reducer
  */
 
 function reducer (state, action) {
-  const {key, initialState = {}} = action.payload
-
   switch (action.type) {
-    case CREATE:
-      return {
-        ...state,
-        ephemeral: {
-          ...(state.ephemeral || {}),
-          [key]: {
-            key: key,
-            state: initialState
-          }
-        }
-      }
+    case CREATE: {
+      const locale = action.meta.locale
+      const path = locale.split('.')
+      localReducers[locale] = action.payload.reducer
+      return update(path, action.payload.initialState, state)
+    }
+    case UPDATE: {
+      const locale = action.meta.locale
+      const path = locale.split('.')
+      const reducer = localReducers[locale]
 
-    case UPDATE:
-      const ephemeral = state.ephemeral[key]
-
-      return {
-        ...state,
-        ephemeral: {
-          ...(state.ephemeral),
-          [key]: {
-            ...state.ephemeral[key],
-            state: localReducers[key](ephemeral.state, action.payload.action)
-          }
-        }
-      }
-
-    case DESTROY:
-      return {
-        ...state,
-        ephemeral: omit(state.ephemeral, key)
-      }
+      return update(path, reducer(get(path, state), action.payload), state)
+    }
+    case DESTROY: {
+      return destroy(action.meta.locale.split('.'), state)
+    }
   }
 
   return state
 }
 
-function actions (localReducer) {
-  return {
-    create (key, initialState) {
-      localReducers[key] = localReducer
+/**
+ * Utilities
+ */
 
-      return {
-        type: CREATE,
-        payload: {key, initialState}
-      }
-    },
+function get (path, state) {
+  return path.reduce((acc, key) => acc[key], state)
+}
 
-    update (key, action) {
-      return {
-        type: UPDATE,
-        payload: {key, action}
-      }
-    },
+function update (path, value = {}, state) {
+  const key = path[0]
 
-    destroy (key) {
-      delete localReducers[key]
-
-      return {
-        type: DESTROY,
-        payload: {key}
-      }
-    }
+  if (path.length === 0) {
+    return value
   }
+
+  return {
+    ...state,
+    [key]: update(path.slice(1), value, state[key])
+  }
+}
+
+function destroy (path, state) {
+  const key = path[0]
+
+  if (path.length === 1) {
+    return omit(state, key)
+  }
+
+  return {
+    ...state,
+    [key]: update(path.slice(1), state[key])
+  }
+}
+
+/**
+ * Action creators
+ */
+
+function createEphemeral (locale, reducer, initialState) {
+  return {
+    type: CREATE,
+    payload: {initialState, reducer},
+    meta: {locale}
+  }
+}
+
+function updateEphemeral (locale, action) {
+  return {
+    type: UPDATE,
+    payload: action,
+    meta: {locale}
+  }
+}
+
+function destroyEphemeral (locale) {
+  return {
+    type: DESTROY,
+    meta: {locale}
+  }
+}
+
+function computePath (locale) {
+  const path = []
+
+  while (locale) {
+    path.push(locale)
+    locale = parents[locale]
+  }
+
+  return path.reverse()
 }
 
 /**
  * Exports
  */
 
-export default {
-  reducer,
-  actions
+export default reducer
+export {
+  createEphemeral,
+  updateEphemeral,
+  destroyEphemeral
 }
